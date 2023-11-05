5f6e644182ff1d86049a70070cbc00a6eb873fb9
araaancg <carpinteroaranchag@gmail.com>
2023-11-05 18:53:57 +0100

    feat: combined data form two different sheets

--- name-hosts/main.py
+++ name-hosts/main.py
@@ -1,11 +1,33 @@
 import openpyxl as xl
 
-doc = xl.load_workbook('./exEmails.xlsx')
+# Load the Excel file
+workbook = xl.load_workbook('excelEjemplo.xlsx')
 
-ws = doc.active
+# Access the sheets
+email_sheet = workbook['emails']
+host_sheet = workbook['hosts']
 
-# for x in range(1,101):
-#     for y in range(1,101):
-#         print(ws.cell(row=x, column=y))
-d = ws.cell(row=2, column=2, value=10)
-print(d)
+# Create a new sheet for the combined data
+combined_sheet = workbook.create_sheet(title='combinedData')
+
+# Extract usernames from email addresses (from column A, skipping the header)
+usernames = [row[0].split('@')[0] for row in email_sheet.iter_rows(min_row=2, min_col=1, max_col=1, values_only=True)]
+
+# Create a dictionary to store the mapping of usernames to hosts
+user_host_mapping = {}
+
+# Iterate through host rows (from column A, skipping the header)
+for row in host_sheet.iter_rows(min_row=2, min_col=1, max_col=1, values_only=True):
+    host = row[0]
+    username = ''.join(filter(str.isalpha, host))
+    if username in usernames:
+        user_host_mapping[username] = host
+
+# Write the email and corresponding host to the combined sheet
+for username in usernames:
+    email = f'{username}@email.com'
+    host = user_host_mapping.get(username, 'No matching host found')
+    combined_sheet.append([email, host])
+
+# Save the workbook
+workbook.save('excelEjemplo.xlsx')
